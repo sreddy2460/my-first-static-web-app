946b35bd0896126693aa48083a8b4b8a682eb252
sreddy2460 <112101970+sreddy2460@users.noreply.github.com>
2022-08-26 22:29:53 +0530

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
 const myHeading = document.querySelector('h1');
 const myImage = document.querySelector('img');
+const heroimage = document.getElementById('heropic');
 
 myHeading.textContent = 'Welcome to Santhosh World .... ';
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute('src');
   if (mySrc === 'mypic.jpg') {
     myImage.setAttribute('src','myimg2.jpg');
+    heroimage.setAttribute('src','myimg2.jpg');
     myHeading.textContent = 'Welcome to Kaushik World .... ';
   } else {
     myImage.setAttribute('src','mypic.jpg');
+    heroimage.setAttribute('src','mypic.jpg');
     myHeading.textContent = 'Welcome to Santhosh World .... ';
   }
 }
