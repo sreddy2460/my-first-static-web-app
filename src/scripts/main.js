28f4245c16b80e0dee9799f29dc443492b13c13d
sreddy2460 <112101970+sreddy2460@users.noreply.github.com>
2022-08-26 22:04:28 +0530

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
+const myHeading = document.querySelector('h1');
+const myImage = document.querySelector('img');
 
+myHeading.textContent = 'Hello world Santhosh in JS!';
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'myimg.jpg') {
+    myImage.setAttribute('src','myimg2.jpg');
+  } else {
+    myImage.setAttribute('src','myimg.jpg);
+  }
+}
