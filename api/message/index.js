1012ac5ea5974fa223767ae1bbe2e36d1f5145e5
sreddy2460 <112101970+sreddy2460@users.noreply.github.com>
2022-08-29 16:24:19 +0530

    Update index.js

--- api/message/index.js
+++ api/message/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     
    const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, This is " + name + "!"
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     context.res.json({
         text: responseMessage
