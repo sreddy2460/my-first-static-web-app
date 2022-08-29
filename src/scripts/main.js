c6c5dafe70b77f3d1e3e069d62cea5d4d74416a2
sreddy2460 <112101970+sreddy2460@users.noreply.github.com>
2022-08-29 06:45:22 +0530

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
+  document.getElementById("loadingtext").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
 
